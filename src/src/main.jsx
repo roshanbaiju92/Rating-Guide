e8e70828d82ad4f1f11940bf1a4b624d0cc019d1
roshanbaiju92 <roshanbaiju92@gmail.com>
2026-04-05 18:11:18 +0530

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RatingGuide from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RatingGuide />
+);
