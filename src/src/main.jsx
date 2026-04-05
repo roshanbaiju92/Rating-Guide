cb078cc01cb07e3a4ec7a8a53b423f5b3a185ef1
roshanbaiju92 <roshanbaiju92@gmail.com>
2026-04-05 18:12:08 +0530

    Delete src/src directory

--- src/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RatingGuide from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RatingGuide />
-);
